39f1b9a8b93769dd25023ee0bb5e38600f2c287e
Krishanu Dey <krishanudey99@gmail.com>
2020-04-18 12:52:42 +0530

    feat(devices controller): adds devices controller with basic methods
    Methods added: discoverDevicesOnNetwork, getAllDevicesFromDb, getDeviceByUuidFromDb,
    getDeviceByNameFromDb, addDeviceToDb, removeDeviceFromDb

---
+++ src/controller/devices-controller.ts
@@ -0,0 +1,174 @@
+import { Request, Response, NextFunction } from "express";
+import { DeviceHelperInstance, DeviceType } from "../service/device-helper";
+import { DatabaseInstance } from "../database/db";
+import * as Joi from "@hapi/joi";
+import {
+    DeviceAlreadyAddedException,
+    DuplicateDeviceNameException,
+} from "../utils/errors";
+import { HTTP_API_ERROR_CODE } from "../utils/custom-error-codes";
+
+const addDeviceSchema = Joi.object().keys({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    uuid: Joi.string().uuid().required(),
+    type: Joi.string()
+        .valid(...Object.keys(DeviceType))
+        .required(),
+    ip: Joi.string().ip().required(),
+    servicePort: Joi.number().min(1).max(65535).required(),
+});
+
+export const findDevices = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const devices = await DeviceHelperInstance.findDevices();
+        const response = devices.map((el: any) => {
+            const added = DatabaseInstance.isDeviceUuidAdded(el.uuid);
+            el.isAdded = added;
+            return el;
+        });
+
+        res.json(response);
+    } catch (err) {
+        res.status(500).json({
+            reason: HTTP_API_ERROR_CODE.INTERNAL_SERVER_ERROR,
+            message: err.message,
+        });
+    }
+};
+
+export const getAllDevices = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const d = DatabaseInstance.getDevices();
+        return res.json(d);
+    } catch (err) {
+        res.status(500).json({
+            reason: HTTP_API_ERROR_CODE.INTERNAL_SERVER_ERROR,
+            message: err.message,
+        });
+    }
+};
+
+export const getDeviceByUuid = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const uuid = req.params.uuid;
+        const validationResult = Joi.string().uuid().required().validate(uuid);
+        if (validationResult.error || validationResult.errors) {
+            return res.status(400).json({
+                reason: HTTP_API_ERROR_CODE.VALIDATION_FAILED,
+                message: "UUID not provided!!!",
+            });
+        }
+        const d = DatabaseInstance.getDeviceByUuid(uuid);
+        if (!d) {
+            return res.status(404).json({
+                reason: HTTP_API_ERROR_CODE.NOT_FOUND,
+                message: "Device not Found!!!",
+            });
+        }
+        return res.json(d);
+    } catch (err) {
+        res.status(500).json({
+            reason: HTTP_API_ERROR_CODE.INTERNAL_SERVER_ERROR,
+            message: err.message,
+        });
+    }
+};
+
+export const getDeviceByName = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const name = req.params.name;
+        if (!name) {
+            return res.status(400).json({
+                reason: HTTP_API_ERROR_CODE.VALIDATION_FAILED,
+                message: "Name not provided!!!",
+            });
+        }
+        const d = DatabaseInstance.getDeviceByName(name);
+        return res.json(d);
+    } catch (err) {
+        res.status(500).json({
+            reason: HTTP_API_ERROR_CODE.INTERNAL_SERVER_ERROR,
+            message: err.message,
+        });
+    }
+};
+
+export const addDevice = (req: Request, res: Response, next: NextFunction) => {
+    const validationResult = addDeviceSchema.validate(req.body);
+    if (validationResult.error) {
+        return res.status(400).json({
+            reason: HTTP_API_ERROR_CODE.VALIDATION_FAILED,
+            message: validationResult.error.message,
+            details: validationResult.error.details,
+        });
+    }
+    const { name, uuid, type, ip, servicePort } = req.body;
+    try {
+        const d = DatabaseInstance.addDevice(name, {
+            uuid,
+            ip,
+            type,
+            servicePort,
+        });
+        return res.json({ message: "Device added successfully", device: d });
+    } catch (err) {
+        if (err instanceof DeviceAlreadyAddedException) {
+            res.status(400).json({
+                reason: HTTP_API_ERROR_CODE.DEVICE_ALREADY_ADDED,
+                message: err.message,
+                details: { name, uuid, type, ip, servicePort },
+            });
+        } else if (err instanceof DuplicateDeviceNameException) {
+            res.status(400).json({
+                reason: HTTP_API_ERROR_CODE.DUPLICATE_DEVICE_NAME,
+                message: err.message,
+                details: { name, uuid, type, ip, servicePort },
+            });
+        } else {
+            res.status(400).json({
+                reason: HTTP_API_ERROR_CODE.INTERNAL_SERVER_ERROR,
+                message: err.message,
+                details: { name, uuid, type, ip, servicePort },
+            });
+        }
+    }
+};
+
+export const removeDevice = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const uuid = req.params.uuid;
+        if (!uuid) {
+            return res.status(400).json({
+                reason: HTTP_API_ERROR_CODE.VALIDATION_FAILED,
+                message: "UUID not provided!!!",
+            });
+        }
+        const d = DatabaseInstance.removeDeviceByUuid(uuid);
+        return res.json({ message: "Device removed successfully", device: d });
+    } catch (err) {
+        res.status(500).json({
+            reason: HTTP_API_ERROR_CODE.INTERNAL_SERVER_ERROR,
+            message: err.message,
+        });
+    }
+};
